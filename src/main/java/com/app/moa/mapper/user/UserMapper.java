8f303b06b72af73c42a4b1be550e1a07e508e480
dev-resetok <746aa1357@gmail.com>
2024-09-23 11:29:52 +0900

    userMapper working

--- src/main/java/com/app/moa/mapper/user/UserMapper.java
+++ src/main/java/com/app/moa/mapper/user/UserMapper.java
@@ -1,4 +1,23 @@
 package com.app.moa.mapper.user;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
+
+@Mapper
 public interface UserMapper {
+    //    회원가입
+    public void insert(UserVO userVO);
+
+    //    로그인
+    public Optional<UserVO> selectByUserEmailAndUserPassword(UserVO userVO);
+
+    //    회원 정보 조회
+    public Optional<UserVO> selectById(Long id);
+
+    //    회원 정보 수정
+    public void update(UserVO userVO);
+
+    //    회원 정보 삭제
+    public void delete(Long id);
 }
