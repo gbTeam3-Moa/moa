04f6443d867fe2aa428a2572de3adbf7d632b0b8
jimin9712 <ljm21000@gmail.com>
2024-09-24 12:27:19 +0900

    post

--- src/main/java/com/app/moa/domain/projectpost/ProjectPostVO.java
+++ src/main/java/com/app/moa/domain/projectpost/ProjectPostVO.java
@@ -1,4 +1,15 @@
 package com.app.moa.domain.projectpost;
 
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProjectPostVO {
+    private Long id;
+
 }
