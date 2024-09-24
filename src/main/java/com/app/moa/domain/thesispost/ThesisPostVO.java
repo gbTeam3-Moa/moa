1d98960e83f377630351684c9896ea16d12401d3
jimin9712 <ljm21000@gmail.com>
2024-09-24 14:51:49 +0900

    project

--- src/main/java/com/app/moa/domain/thesispost/ThesisPostVO.java
+++ src/main/java/com/app/moa/domain/thesispost/ThesisPostVO.java
@@ -1,4 +1,16 @@
 package com.app.moa.domain.thesispost;
 
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class ThesisPostVO {
+    private Long id;
+
 }
