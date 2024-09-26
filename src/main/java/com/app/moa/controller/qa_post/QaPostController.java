52046728e8f4d61fb23a249cc02228d82628e7b0
hmk4079 <hmk4079@gmail.com>
2024-09-26 14:49:29 +0900

    init

--- src/main/java/com/app/moa/controller/qa_post/QaPostController.java
+++ src/main/java/com/app/moa/controller/qa_post/QaPostController.java
@@ -1,45 +1,45 @@
-//
-//package com.app.moa.controller.qa_post;
-//
-//import com.app.moa.domain.member.MemberVO;
-//import com.app.moa.domain.post.PostVO;
-//import com.app.moa.domain.qa_post.QaPostDTO;
-//import com.app.moa.domain.post.Pagination;
-//import com.app.moa.service.qa_post.QaPostService;
-//import jakarta.servlet.http.HttpSession;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import java.nio.file.Files;
-//
-//@Controller
-//@RequestMapping("/qa/*")
-//@RequiredArgsConstructor
-//@Slf4j
-//public class QaPostController {
-//    private final QaPostService qaPostService;
-//    private final HttpSession session;
-//
-//    @GetMapping("qa-list")
-//    public void getList(Pagination pagination, Model model) {
-//        pagination.setTotal(qaPostService.getTotal());
-//        pagination.progress();
-//        model.addAttribute("pagination", pagination);
-//        model.addAttribute("posts", qaPostService.getList(pagination));
-//    }
-//
-//    @GetMapping("qa-write")
-//    public void goToWriteForm(QaPostDTO qapostDTO){;}
-//
-//    @PostMapping("qa-write")
-//    public void write(QaPostDTO qapostDTO){
-//        qapostDTO.setMemberId(((MemberVO) session.getAttribute("member")).getId());
-//        qaPostService.write(qapostDTO);
-//    }
-//}
-//
+
+package com.app.moa.controller.qa_post;
+
+import com.app.moa.domain.member.MemberVO;
+import com.app.moa.domain.post.PostVO;
+import com.app.moa.domain.qa_post.QaPostDTO;
+import com.app.moa.domain.post.Pagination;
+import com.app.moa.service.qa_post.QaPostService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.nio.file.Files;
+
+@Controller
+@RequestMapping("/qa/*")
+@RequiredArgsConstructor
+@Slf4j
+public class QaPostController {
+    private final QaPostService qaPostService;
+    private final HttpSession session;
+
+    @GetMapping("qa-list")
+    public void getList(Pagination pagination, Model model) {
+        pagination.setTotal(qaPostService.getTotal());
+        pagination.progress();
+        model.addAttribute("pagination", pagination);
+        model.addAttribute("posts", qaPostService.getList(pagination));
+    }
+
+    @GetMapping("qa-write")
+    public void goToWriteForm(QaPostDTO qapostDTO){;}
+
+    @PostMapping("qa-write")
+    public void write(QaPostDTO qapostDTO){
+        qapostDTO.setMemberId(((MemberVO) session.getAttribute("member")).getId());
+        qaPostService.write(qapostDTO);
+    }
+}
+
