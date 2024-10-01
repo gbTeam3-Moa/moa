e059fae5f89608866fea615cb6d7e92424002f1f
jimin9712 <ljm21000@gmail.com>
2024-10-01 15:56:20 +0900

    gg

--- src/main/resources/static/js/thesis/thesis-list.js
+++ src/main/resources/static/js/thesis/thesis-list.js
@@ -39,7 +39,7 @@ const showList = () => {
                                 ${post.professorMajor || '학과 정보 없음'} 
                             </div>
                             <div class="post-writer-name">
-                                ${member.memberNickName}
+                                ${post.memberNickName}
                             </div>
                             <div class="post-created-date">
                                 ${post.updatedDate || '날짜 정보 없음'} 
